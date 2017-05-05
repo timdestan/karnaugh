f6c1e7628e2ef679f102f9d13b84a37c0a0929d1
Tim Destan <tim@destan.name>
2017-05-04 23:02:53 -0400

    Another trivial rename

--- src/main/scala/karnaugh/PrettyPrint.scala
+++ src/main/scala/karnaugh/PrettyPrint.scala
@@ -23,30 +23,30 @@ object Tree {
     override def toString = s"¬${child.toString}"
   }
 
-  case class And(children: List[Tree], ctx: Context) extends Tree {
+  case class And(children: List[Tree], parentContext: Context) extends Tree {
     def renderedChildren = children.mkString(" ∧ ")
 
-    override def toString = ctx match {
+    override def toString = parentContext match {
       case CNot | COr => wrapInParens(renderedChildren)
       case CTop | CAnd => renderedChildren
     }
   }
 
-  case class Or(children: List[Tree], ctx: Context) extends Tree {
+  case class Or(children: List[Tree], parentContext: Context) extends Tree {
     def renderedChildren = children.mkString(" ∨ ")
 
-    override def toString = ctx match {
+    override def toString = parentContext match {
       case CNot | CAnd => wrapInParens(renderedChildren)
       case CTop | COr => renderedChildren
     }
   }
 
   def apply(exp: Exp): Tree = {
-    def loop(exp: Exp, ctx: Context): Tree = exp match {
+    def loop(exp: Exp, parentContext: Context): Tree = exp match {
       case Exp.Variable(v) => Atom(v)
       case Exp.Not(e) => Not(loop(e, CNot))
-      case Exp.Or(es) => Or(es.map(loop(_, COr)), ctx)
-      case Exp.And(es) => And(es.map(loop(_, CAnd)), ctx)
+      case Exp.Or(es) => Or(es.map(loop(_, COr)), parentContext)
+      case Exp.And(es) => And(es.map(loop(_, CAnd)), parentContext)
       case Exp.Literal(v) => Atom(v.toString)
     }
     loop(exp, CTop)
